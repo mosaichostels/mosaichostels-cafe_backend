48286435098ed34dbd9a3dd0828ad946f22b7b88
mosaichostels <mosaichostels@gmail.com>
2026-03-15 07:46:09 +0530

    Code Changes

--- src/main/java/com/hostel/ordering/exception/ResourceNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.hostel.ordering.exception;
-
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
